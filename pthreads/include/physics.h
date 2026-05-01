14d916eb1fea6ab36c396628b2a76fe6b63ee798
Daniel Mihovch <dmihovch@gmail.com>
2026-05-01 12:39:11 -0400

    switch pthreads to SoA

--- pthreads/include/physics.h
+++ pthreads/include/physics.h
@@ -4,13 +4,15 @@
 #include "constants.h"
 #include "maths.h"
 #include <math.h>
-void update_bodies(Body* b, int nbodies);
-void reset_accelerations(Body* p, int nbodies);
-void accumulate_forces(Body* p, int nbodies);
-void move_bodies_handle_wall_collisions(Body* p, int nbodies);
-void handle_body_body_collisions(Body* p, int nbodies);
-Vector2 check_collisions_circles(float* scalar_dist,Vector2 apos, float ar, Vector2 bpos, float br);
-float calculate_impulse(Body a, Body b, Vector2 normal);
+
+void update_bodies(Bodies* b);
+void reset_accelerations(Bodies* b);
+void accumulate_forces(Bodies* b);
+void move_bodies_handle_wall_collisions(Bodies* b);
+void handle_body_body_collisions(Bodies* b);
+Vector2 check_collisions_circles(float* scalar_dist, Vector2 apos, float ar, Vector2 bpos, float br);
+float calculate_impulse(Vector2 a_vel, Vector2 b_vel, float a_m, float b_m, Vector2 normal);
 bool collision_occured(Vector2 normal);
-void handle_penetration(Body* a, Body* b,Vector2 normal, float scalar_distance);
-#endif //PHYSICS_H
+void handle_penetration(Vector2* a_pos, Vector2* b_pos, float a_r, float b_r, float a_m, float b_m, Vector2 normal, float scalar_distance);
+
+#endif
